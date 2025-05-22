0f6e7e44a415807ac05168fbd7ebd9abe88e5df7
hellitonsmate <154920199+hellitonsmate@users.noreply.github.com>
2025-05-21 22:18:54 -0400

    Create ldap_utils.py

---
+++ ldap_utils.py
@@ -0,0 +1,21 @@
+import ldap
+
+def connect_ldap(config):
+    conn = ldap.initialize(config['LDAP_SERVER_URI'])
+    conn.simple_bind_s(config['LDAP_BIND_DN'], config['LDAP_BIND_PASSWORD'])
+    return conn
+
+def get_all_users(conn, base_dn, attributes=['uid', 'cn', 'mail']):
+    # Cuidado: O filtro '(objectClass=inetOrgPerson)' pode variar conforme seu schema
+    search_filter = "(objectClass=inetOrgPerson)" # Ou person, posixAccount, etc.
+    try:
+        results = conn.search_s(base_dn, ldap.SCOPE_SUBTREE, search_filter, attributes)
+        users = []
+        for dn, entry in results:
+            user_data = {attr.decode(): entry[attr][0].decode() for attr in entry if entry[attr]}
+            user_data['dn'] = dn.decode() # Pode ser útil
+            users.append(user_data)
+        return users
+    except ldap.LDAPError as e:
+        print(f"LDAP Error: {e}")
+        return []
